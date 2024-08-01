9efb69f6c7e2165fde728e21e444df74cd6ca14c
Diego Reyes <diegorele13@gmail.com>
2024-08-01 11:42:20 -0500

    add: user validator

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -15,41 +15,6 @@ export const userValidatorCreate = [
     .optional()
     .isString()
     .withMessage('Name must be a string'),
-
-  check('CUIL')
-    .optional()
-    .matches(/^\d{2}-\d{8}-\d{1}$|^\d{11}$/)
-    .withMessage('CUIL is not valid'),
-
-  check('CUIT')
-    .optional()
-    .matches(/^\d{2}-\d{8}-\d{1}$|^\d{11}$/)
-    .withMessage('CUIT is not valid'),
-
-  check('benefitNumber')
-    .optional()
-    .isString()
-    .withMessage('Benefit Number must be a string'),
-    
-  check('email')
-    .optional()
-    .isEmail()
-    .withMessage('Email is not valid'),
-
-  check('age')
-    .optional()
-    .isInt({ min: 0 })
-    .withMessage('Age must be a non-negative integer'),
-
-  check('financialData')
-    .optional()
-    .isMongoId()
-    .withMessage('Financial Data must be a valid Mongo ID'),
-
-  check('credit')
-    .optional()
-    .isMongoId()
-    .withMessage('Credit must be a valid Mongo ID'),
     
   (req: Request, res: Response, next: NextFunction) => {
     return validateResults(req, res, next);
